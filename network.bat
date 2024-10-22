3835e904ec8620fa14a91365899d590650172a32
Tiago-2007 <tiago.machado.3105@gmail.com>
2024-10-22 09:33:38 +0100

    Added File/App Assoc

--- network.bat
+++ network.bat
@@ -8,12 +8,13 @@ echo ##############################################################
 echo # (c) . 2024. Tiago Machado
 echo ##############################################################
 
-echo 1- Ip configurations
-echo 2- Renew IP configurations
-echo 3- DNS_QUERY
-echo 4- Show MAC address 
-echo 5- enery efficent
-echo 9- Exit
+echo 1 - Ip configurations
+echo 2 - Renew IP configurations
+echo 3 - DNS_QUERY
+echo 4 - Show MAC address 
+echo 5 - Energy efficent
+echo 6 - File/Aplication Association
+echo 9 - Exit
 
 set /p opt=Select an option then press ENTER: 
 
@@ -21,8 +22,8 @@ if %opt% == 1 goto IP_CONFIGURATIONS
 if %opt% == 2 goto RENEW_IP_CONFIGURATIONS
 if %opt% == 3 goto DNS_QUERY
 if %opt% == 4 goto SHOW_MAC_ADDRESS
-if %opt% == 5 goto SHOW_POWERCFG /ENERGY
-if %opt% == 6 goto ENERGY_EFFICIENT
+if %opt% == 5 goto ENERGY_EFFICIENT
+if %opt% == 6 goto FILE_APP_ASSOC
 if %opt% == 9 goto EXIT_SCRIPT
 
 goto MENU
@@ -55,6 +56,10 @@ Powercfg /enery
 pause
 goto MENU
 
+:FILE_APP_ASSOC
+assoc > output.txt
+pause
+goto MENU
 
 :EXIT_SCRIPT
 echo Thanks!
